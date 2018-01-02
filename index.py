f7be4686b961411b62648372474ae9e54669c043
nhornacek <nickhornacek@gmail.com>
2018-01-02 18:03:18 -0500

    Update index.py

--- index.py
+++ index.py
@@ -1,12 +1,4 @@
-import json
-import datetime
-
-
-def handler(event, context):
-    data = {
-        'output': 'Hello World',
-        'timestamp': datetime.datetime.utcnow().isoformat()
-    }
-    return {'statusCode': 200,
-            'body': json.dumps(data),
-            'headers': {'Content-Type': 'application/json'}}
+import requests
+r = requests.get('https://alphavantage.com/query?function=BATCH_STOCK_QUOTES&symbols=PRED&apikey=QTNISIBHR5PBVIHR')
+predprice = r["price"]
+print predprice
